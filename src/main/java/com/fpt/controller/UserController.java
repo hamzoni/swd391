d711c0d3603a1befbbdcbe16e0be290f06b73354
DucHT4 <duchoang191@gmail.com>
2018-07-21 22:59:45 +0700

    login 2 import data from body

--- src/main/java/com/fpt/controller/UserController.java
+++ src/main/java/com/fpt/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,6 +35,12 @@ public class UserController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ResponseEntity<Users> login(@RequestParam(value = "username") String user,
 			@RequestParam(value = "password") String password) {
+		return new ResponseEntity<>(userService.login(user, password), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/login2", method = RequestMethod.GET)
+	public ResponseEntity<Users> login2(@PathVariable(value = "username") String user,
+			@PathVariable(value = "password") String password) {
 		return new ResponseEntity<>(userService.login(user, password), HttpStatus.OK);
 	}
 }
