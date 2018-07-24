8257938c4c8b350294e4bc91584ea4464b383f77
TungLT25 <tunglt25@fsoft.com.vn>
2018-07-25 01:44:42 +0700

    - configure

--- src/main/java/com/fpt/SwdDemoApplication.java
+++ src/main/java/com/fpt/SwdDemoApplication.java
@@ -2,10 +2,17 @@ package com.fpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SwdDemoApplication {
+public class SwdDemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SwdDemoApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SwdDemoApplication.class, args);
 	}
